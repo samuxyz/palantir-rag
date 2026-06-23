ae3387f169e11aba8a23f9987163afee32415c2b
samuxyz <zaza.samuele@gmail.com>
2026-06-23 11:45:42 +0800

    whitelisted vercel url for client connection

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://palantir-oracle.vercel.app",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
